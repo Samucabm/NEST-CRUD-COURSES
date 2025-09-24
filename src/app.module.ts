b83354ffe30d5a35ca4fecbb70c39e81095d0bc1
ADS 2024.2N <36129382024.2N@senacrjedu.local>
2025-09-23 21:28:32 -0300

    prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoursesService } from './courses/courses.service';
+import { CoursesController } from './courses/courses.controller';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CoursesModule],
+  controllers: [AppController, CoursesController],
+  providers: [AppService, CoursesService],
 })
 export class AppModule {}
